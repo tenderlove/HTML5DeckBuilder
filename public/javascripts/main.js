2ad46bc1d4ac7cfa6f38769997e1437329843cf4
Aaron Patterson <aaron.patterson@gmail.com>
2014-03-15 17:21:13 -0700

    we can save and load decks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,45 @@
 var dragSrcEl = null;
 
-function Deck(cards) { this.cards = cards; }
+function Table(root) {
+  this.root = root;
+}
 
-Deck.prototype.handleDragStart = function(e) {
+Table.prototype.rowTypes =
+    ["creatures", "spells", "artifacts", "enchantments", "lands", "planeswalkers"];
+
+Table.prototype.rowTypes.forEach(function(thing) {
+  Table.prototype[thing] = function() {
+    return $("div." + thing + " img", this.root).map(function() {
+      return jQuery.data(this, "card");
+    }).toArray();
+  }
+});
+
+Table.prototype.clear = function() {
+  this.rowTypes.forEach(function(type) {
+    var node = $("div." + type, this.root)[0];
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+  });
+};
+
+Table.prototype.handleDragStart = function(e) {
   this.style.opacity = '0.4';
   dragSrcEl = this;
   e.dataTransfer.effectAllowed = 'move';
-}
+};
 
-Deck.prototype.handleDragEnd = function(e) {
+Table.prototype.handleDragEnd = function(e) {
   this.style.opacity = '1.0';
 };
 
-Deck.prototype.handleDragOver = function(e) {
+Table.prototype.handleDragOver = function(e) {
   if (e.preventDefault) { e.preventDefault(); }
   return false;
 };
 
-Deck.prototype.addCard = function(card, loc) {
+Table.prototype.addCard = function(card, loc) {
   var div = document.createElement('div');
   var img = document.createElement('img');
   var deck = this;
@@ -45,11 +67,11 @@ Deck.prototype.addCard = function(card, loc) {
   alignImages();
 }
 
-Deck.prototype.isMember = function(img) {
+Table.prototype.isMember = function(img) {
   return $(img).parents("div.deck")[0];
 }
 
-Deck.prototype.removeCard = function(img) {
+Table.prototype.removeCard = function(img) {
   var div = img.parentNode;
   div.parentNode.removeChild(div);
   alignImages();
@@ -109,8 +131,88 @@ function addFour(deck) {
   return false;
 }
 
+var allDecks = [
+  {
+    'name': 'Deck one',
+    'creatures': [373543, 373543, 373543, 370688],
+    'spells': [369041]
+  }
+];
+
+var mvidToCards = {};
+
+function addDeckList(savedDecks, form) {
+  var select = $("select", form);
+  var node = select[0];
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+
+  // Default empty option
+  var option = document.createElement("option");
+  option.text = "";
+  option.value = "";
+  select.append(option);
+
+  savedDecks.forEach(function(deck) {
+    var option = document.createElement("option");
+    option.text = deck['name'];
+    option.value = deck['name'];
+    select.append(option);
+  });
+}
+
+function saveDeck(storage, deck, name) {
+  var existingDeck = storage.filter(function(storedDeck) {
+    return storedDeck.name == name;
+  })[0];
+
+  var mvidfun = function(x) { return x.multiverseid; };
+  var storedDeck = {}
+
+  if (existingDeck) { storedDeck = existingDeck }
+
+  storedDeck.name          = name;
+  storedDeck.creatures     = deck.creatures().map(mvidfun);
+  storedDeck.spells        = deck.spells().map(mvidfun);
+  storedDeck.artifacts     = deck.artifacts().map(mvidfun);
+  storedDeck.enchantments  = deck.enchantments().map(mvidfun);
+  storedDeck.lands         = deck.lands().map(mvidfun);
+  storedDeck.planeswalkers = deck.planeswalkers().map(mvidfun);
+
+  console.log(storedDeck);
+  console.log(storage);
+
+  if (!existingDeck) { storage.push(storedDeck); }
+  addDeckList(storage, $("#deck-controls"));
+}
+
+function loadDeck(storage, name, table, mvidToCards) {
+  var deck = storage.filter(function(storedDeck) {
+    return storedDeck.name == name;
+  })[0];
+
+  var tableSections = {
+    creatures: $("div.creatures"),
+    spells: $("div.spells"),
+    artifacts: $("div.artifacts"),
+    enchantments: $("div.enchantments"),
+    lands: $("div.lands"),
+    planeswalkers: $("div.planeswalkers"),
+  }
+
+  for (var section in tableSections) {
+    (deck[section] || []).forEach(function(mvid) {
+      table.addCard(mvidToCards[mvid], tableSections[section]);
+    });
+  }
+}
+
+var table;
+
 $(document).ready(function() {
-  var deck = new Deck([]);
+  var deck = new Table($("div.deck")[0]);
+  table = deck;
   var trash = document.querySelector('div.trash');
   trash.addEventListener('dragover', handleDragOver, false);
   trash.addEventListener('drop', function (e) {
@@ -120,7 +222,39 @@ $(document).ready(function() {
   }, false);
   $("#addone").click(function() { return addOne(deck); });
   $("#addfour").click(function() { return addFour(deck); });
+
+  $("#load").click(function() {
+    var deckName = $("select", $(this).parents("form")).val();
+    if (deckName != '') {
+      deck.clear();
+      loadDeck(allDecks, deckName, deck, mvidToCards);
+    }
+    return false;
+  });
+
+  $("#save").click(function() {
+    var form       = $(this).parents("form");
+    var selectName = $("select", form).val();
+    var textName   = $("input", form).val();
+
+    var deckName = null;
+
+    if (selectName == "" && textName == "") {
+      deckName = "New Deck";
+    } else if (selectName == "" || textName != "") {
+      deckName = textName;
+    } else {
+      deckName = selectName;
+    }
+
+    console.log(deckName);
+    saveDeck(allDecks, deck, deckName);
+    return false;
+  });
+
   var cols = document.querySelectorAll("div.deck > div.boardlist > div");
+
+  addDeckList(allDecks, $("#deck-controls"));
 
   // Drop cards in particular columns
   [].forEach.call(cols, function(col) {
@@ -149,13 +283,19 @@ $(document).ready(function() {
     var options = cards.map(function(card) {
       var option = document.createElement('option');
       option.value = card.multiverseid;
-      card.imgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + card.multiverseid + "&type=card";
       option.innerHTML = card.name;
       jQuery.data(option, "card", card);
       return option;
     });
 
     options.forEach(function(option) { selectList.append(option); });
+
+    for(var key in data) {
+      data[key].cards.forEach(function(card) {
+        card.imgUrl = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" + card.multiverseid + "&type=card";
+        mvidToCards[card.multiverseid] = card;
+      })
+    }
 
     $("form input[name='filter']").change(function () {
       var txt = options[0].text.toLowerCase();
